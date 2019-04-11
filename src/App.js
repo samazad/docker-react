e35a7934f21f99135c254abfa53037779e055667
Sameer Azad <sazad@cisco.com>
2019-04-11 18:39:56 -0400

    Created Feature Branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello from my first React App deployed on AWS using Travis-CI and EB!!
+            I was changed on the feature branch!!
           </p>
           <a
             className="App-link"
